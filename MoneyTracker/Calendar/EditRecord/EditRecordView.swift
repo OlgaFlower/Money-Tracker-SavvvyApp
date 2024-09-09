c88c7ff20825355f2f7d25d50eff553b88db8389
Olha Bereziuk <olhaber.dev@gmail.com>
2024-09-09 19:29:43 +0200

    [CHANGE] Edit Record View UI

--- MoneyTracker/Calendar/EditRecord/EditRecordView.swift
+++ MoneyTracker/Calendar/EditRecord/EditRecordView.swift
@@ -15,7 +15,7 @@ struct EditRecordView: View {
     @StateObject private var viewModel = EditRecordViewModel()
     @State var sum = "0,00 €"
     @State var category = Category(moneyGroupType: .none, name: "category", icon: "sun.min")
-    @State var note = "Some note here"
+    @State var note = "add note..."
     @State private var selectedDate: Date
     @State private var isDatePickerPresented = false
     
@@ -28,106 +28,94 @@ struct EditRecordView: View {
     var body: some View {
         ZStack {
             BackgroundGradView()
+            
             VStack {
                 CancelButtonView(action: { dismiss() })
-                TextHeaderView(text: "EDIT")
+                TextHeaderView(text: "editing")
                     .padding(.top, 16)
                 
                 VStack(spacing: 32) {
-                    self.dateEditorView
-                        .onTapGesture {
-                            self.isDatePickerPresented.toggle()
-                        }
+                    self.makeDateEditor()
                     self.sumView
-                    self.noteView
                     self.categoryView
+                    self.noteView
                 }
-                .padding(.top, 32)
-                self.updateButtonView
-                    .padding(.top, 48)
+                .padding([.top, .horizontal], 50)
+                
                 Spacer()
+                self.saveButtonView
             }
             .padding(.top, 16)
             .blur(radius: isDatePickerPresented ? 10 : 0)
             
-            if isDatePickerPresented {
-                Rectangle()
-                    .fill(.darkBlue.opacity(0.6))
-                    .ignoresSafeArea()
-                    
-                DatePickerView(
-                    recordTimestamp: $selectedDate,
-                    isPresented: $isDatePickerPresented
-                )
-                .padding(.horizontal, 32)
-                .transition(.scale)
-            }
+            self.datePickerView
         }
         .foregroundStyle(.white)
         .animation(.easeInOut(duration: 0.4), value: self.isDatePickerPresented)
     }
     
     // MARK: - Views
-    /// Date
-    private var dateEditorView: some View {
-        ZStack {
-            BackgroundRectangleView(heigt: 50)
-            HStack {
-                TextView(text: "Date")
-                Spacer()
-                DateTitleView(
-                    date: self.$selectedDate,
-                    style: .regular,
-                    size: .body
-                )
-                .padding(.vertical, 6)
-                .padding(.horizontal, 12)
-                .background {
-                    RoundedRectangle(cornerRadius: 4, style: .continuous)
-                        .fill(.black.opacity(0.15))
+    // Date Picker
+    @ViewBuilder
+    private var datePickerView: some View {
+        if isDatePickerPresented {
+            Rectangle()
+                .fill(.darkBlue.opacity(0.6))
+                .ignoresSafeArea()
+            
+            DatePickerView(
+                recordTimestamp: $selectedDate,
+                isPresented: $isDatePickerPresented
+            )
+            .padding(.horizontal, 32)
+            .transition(.scale)
+        }
+    }
+    // Date
+    private func makeDateEditor() -> some View {
+        HStack {
+            self.dateEditorView
+                .onTapGesture {
+                    self.isDatePickerPresented.toggle()
                 }
-            }
-            .padding(.horizontal, 32)
+            Spacer()
         }
-        .foregroundStyle(.white)
     }
-    /// Sum
+    private var dateEditorView: some View {
+        DateTitleView(
+            date: self.$selectedDate,
+            style: .regular,
+            size: .body
+        )
+        .padding()
+    }
+    // Sum
     private var sumView: some View {
-        ZStack {
-            BackgroundRectangleView(heigt: 50)
-            HStack {
-                TextHeaderView(text: self.sum, centerHeader: true)
-            }
-            .padding(.horizontal, 18)
+        HStack {
+            TextHeaderView(text: self.sum)
+            Spacer()
         }
     }
-    /// Notes
+    // Notes
     private var noteView: some View {
-        ZStack {
-            BackgroundRectangleView(heigt: 50)
-            HStack {
-                Text(self.note)
-                    .font(.customFont(style: .regular, size: .body))
-                Spacer()
-            }
-            .padding(.horizontal, 32)
+        HStack {
+            SmallTextView(text: self.note)
+            Spacer()
         }
+        .padding([.leading, .top])
     }
-    /// Category
+    // Category
     private var categoryView: some View {
-        ZStack {
-            BackgroundRectangleView(heigt: 50)
-            HStack {
-                Image(systemName: self.category.icon)
-                    .padding(.trailing)
-                TextTitleView(text: self.$category.name, style: .regular)
-                Spacer()
-            }
-            .padding(.horizontal, 32)
+        HStack {
+            Image(systemName: self.category.icon)
+                .padding(.trailing)
+            TextTitleView(text: self.$category.name, style: .regular)
+            Spacer()
         }
+        .padding([.leading, .top])
     }
-    /// Update
-    private var updateButtonView: some View {
+    // Save
+    private var saveButtonView: some View {
         Button {
             self.viewModel.vibrateMedium()
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
@@ -138,16 +126,15 @@ struct EditRecordView: View {
                 }
             }
         } label: {
-            TextView(text: "update", style: .medium)
+            TextView(text: "save", style: .medium)
                 .padding(.vertical, 10)
                 .frame(width: 130)
-                .clipShape(.rect(cornerRadius: 8, style: .continuous))
         }
-        .background {
-            /// Border
-            RoundedRectangle(cornerRadius: 8, style: .continuous)
-                .fill(.lightBlue.opacity(0.5))
-        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 6, style: .continuous)
+                .stroke(.white, lineWidth: 0.5)
+        )
+        .padding(.bottom, 100)
     }
 }
 
